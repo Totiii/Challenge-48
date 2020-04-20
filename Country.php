a18fe700402fef94cbd4279161be3fce18558741
Mathieu <warzcube@gmail.com>
2020-04-20 15:24:30 +0200

    wip actus change country page

--- Country.php
+++ Country.php
@@ -1,6 +1,13 @@
 <?php
 include 'header.php';
 
+function url_encode($string){
+    $encode = str_replace(' ', '%20', $string);
+    $encode = str_replace('(', '%28', $encode);
+    $encode = str_replace(')', '%29', $encode);
+    return $encode;
+}
+
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
@@ -32,7 +39,8 @@ include 'footer.php';
 
 ?>
 
-<body>
+
+
 <div class="container mt-3">
     <div class="jumbotron jumbotron-fluid">
         <div class="container text-center">
@@ -48,9 +56,67 @@ include 'footer.php';
     </div>
 </div>
 
-</body>
 
-<script>
+<?php
 
-</script>
+echo Locale::getDisplayRegion('IT', 'fr');
+
+$actu_contry = url_encode($actualCountryGlobalStats['Country']);
+
+$url_contry_info = "https://restcountries.eu/rest/v2/name/".$actu_contry;
+$curl_contry_info = curl_init();
+curl_setopt($curl_contry_info, CURLOPT_URL, $url_contry_info);
+curl_setopt($curl_contry_info, CURLOPT_RETURNTRANSFER, 1);
+$res_contry_info = json_decode(curl_exec($curl_contry_info));
+curl_close($curl_contry_info);
+var_dump( $res_contry_info);
+$contry_french_name = $res_contry_info[0]->translations->fr;
+$contry_french_name_urlencoded = url_encode($res_contry_info[0]->translations->fr);
+
+    $url_actu_contry = "http://newsapi.org/v2/everything?apiKey=5a358c61c5134605a6a9e3169d9f5abb&sortBy=publishedAt&qinTitle=%28%20coronavirus%20OR%20covid19%20%29%20AND%20".$actu_contry;
+    $url_actu_contry_fr = "http://newsapi.org/v2/everything?apiKey=5a358c61c5134605a6a9e3169d9f5abb&sortBy=publishedAt&language=fr&qinTitle=%28%20coronavirus%20OR%20covid19%20%29%20AND%20".$contry_french_name_urlencoded;
+
+    $curl_actu_contry_fr = curl_init();
+    curl_setopt($curl_actu_contry_fr, CURLOPT_URL, $url_actu_contry_fr);
+    curl_setopt($curl_actu_contry_fr, CURLOPT_RETURNTRANSFER, 1);
+    $res_actu_contry_fr = json_decode(curl_exec($curl_actu_contry_fr));
+    curl_close($curl_actu_contry_fr);
+    $nb_result_fr = $res_actu_contry_fr->totalResults;
+    $nb_result_all_lg = $nb_result_fr;
+var_dump($res_actu_contry_fr);
+    if($nb_result_fr <= 3){
+        //aucun resutat fr
+        $curl_actu_contry = curl_init();
+        curl_setopt($curl_actu_contry, CURLOPT_URL, $url_actu_contry);
+        curl_setopt($curl_actu_contry, CURLOPT_RETURNTRANSFER, 1);
+        $res_actu_contry = json_decode(curl_exec($curl_actu_contry));
+        curl_close($curl_actu_contry);
+
+        $nb_result_all_lg += $res_actu_contry->totalResults;
+
+    }
+
+    var_dump($res_actu_contry);
+
+?>
+
+<div class="container">
+
+    <?php
+
+        if($nb_result_all_lg > 0){
+
+
+
+        }else{
+            ?>
+    <div class="alert alert-primary" role="alert">
+        Aucun article de presse à propos du coronavirus en rapport avec ce pays n'a été trouvé.
+        <a href="https://google.fr/search?q=coronavirus%20en%20<?= $actu_contry ?>" target="_blank">Rechercher sur Google</a>
+    </div>
+    <?php
+        }
+    ?>
+
+</div>
 
--- actu.php
+++ actu.php
@@ -12,29 +12,30 @@ curl_close($curl);
 
 
 
-$headline_url = "http://newsapi.org/v2/top-headlines?country=fr&category=health&apiKey=5a358c61c5134605a6a9e3169d9f5abb&query=coronavirus";
+$headline_url = "http://newsapi.org/v2/top-headlines?country=fr&category=health&pageSize=5&apiKey=5a358c61c5134605a6a9e3169d9f5abb&query=coronavirus";
 $curl_headline = curl_init();
 curl_setopt($curl_headline, CURLOPT_URL, $headline_url);
 curl_setopt($curl_headline, CURLOPT_RETURNTRANSFER, 1);
 $live_news = json_decode(curl_exec($curl_headline));
 curl_close($curl_headline);
 
+$untrusted_sources = array("Jeanmarcmorandini.com");
+
 
 ?>
-<div class="container mt-3">
+<div class="container-fluid mt-3">
     <div class="jumbotron">
         <h1><div class="spinner-grow text-danger display-4" role="status"></div> En direct</h1>
-        <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr class="my-4">
+        <p class="lead">Dernières informations des médias français à propos du Coronavirus</p>
         <div class="row">
         <?php
         foreach ($live_news->articles as $news){
-            if($news->source->name != "Jeanmarcmorandini.com"){
+            if(!in_array($news->source->name, $untrusted_sources)){
             ?>
-                <div class="col-sm-6">
+                <div class="col-sm-4 mb-4">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title"><?= $news->title ?></h5>
+                            <h5 class="card-title"><?= $news->title ?></h5><span class="badge badge-light"><?= $news->publishedAt ?></span>
                             <p class="card-text"><?= $news->description != null?$news->description:$news->content ?></p>
                             <a href="<?= $news->url ?>" target="_blank" class="btn btn-primary">En savoir plus</a>
                         </div>
@@ -51,7 +52,19 @@ curl_close($curl_headline);
 
     <div class="row">
         <div class="col">
-            sdf
+            <div class="card text-center">
+                <div class="card-header">
+                    Economie
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                <div class="card-footer text-muted">
+                    2 days ago
+                </div>
+            </div>
         </div>
         <div class="col">
 sdf
