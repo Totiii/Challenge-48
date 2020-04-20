9bd0492460e3d330131f855f24e91d20de7869cc
Mathieu <warzcube@gmail.com>
2020-04-20 13:24:19 +0200

    wip actu

--- actu.php
+++ actu.php
@@ -10,14 +10,39 @@ curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 $output = curl_exec($curl);
 curl_close($curl);
 
+
+
+$headline_url = "http://newsapi.org/v2/top-headlines?country=fr&category=health&apiKey=5a358c61c5134605a6a9e3169d9f5abb&query=coronavirus";
+$curl_headline = curl_init();
+curl_setopt($curl_headline, CURLOPT_URL, $headline_url);
+curl_setopt($curl_headline, CURLOPT_RETURNTRANSFER, 1);
+$live_news = json_decode(curl_exec($curl_headline));
+curl_close($curl_headline);
+
+
 ?>
 <div class="container mt-3">
     <div class="jumbotron">
         <h1><div class="spinner-grow text-danger display-4" role="status"></div> En direct</h1>
         <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
         <hr class="my-4">
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-        <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        <div class="row">
+        <?php
+        foreach ($live_news->articles as $news){
+            if($news->source->name != "Jeanmarcmorandini.com"){
+            ?>
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $news->title ?></h5>
+                            <p class="card-text"><?= $news->description != null?$news->description:$news->content ?></p>
+                            <a href="<?= $news->url ?>" target="_blank" class="btn btn-primary">En savoir plus</a>
+                        </div>
+                    </div>
+                </div>
+
+        <?php }  }  ?>
+        </div>
     </div>
 
 </div>
